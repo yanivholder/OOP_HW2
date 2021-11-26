15e3e5daa8c6fd3e72fae3655fe8aba7e4a35cd4
eilon299 <eilonkornboim@gmail.com>
2021-11-26 15:39:39 +0200

    done

--- src/OOP2/Solution/FaceOOPImpl.java
+++ src/OOP2/Solution/FaceOOPImpl.java
@@ -7,117 +7,7 @@ import java.util.HashSet;
 
 public class FaceOOPImpl implements FaceOOP, Iterable<Person> {
 
-//    public static class Graph{
-//        class Node{
-//            Person person;
-//            boolean visited;
-//            Integer dist;
-//
-//            public Node(Person person){
-//                person = person;
-//                visited = false;
-//                dist = 0;
-//            }
-//        }
-//        private Map<Node, List<Node> > map = new HashMap<>();
-//        private Set<Node> nodes = new HashSet<>();
-//
-//        public void addVertex(Node s)
-//        {
-//            map.put(s, new LinkedList<Node>());
-//        }
-//
-//        public void addEdge(Node source, Node destination)
-//        {
-//
-//            if (!map.containsKey(source)){
-//                addVertex(source);
-//            }
-//            if (!map.containsKey(destination)){
-//                addVertex(destination);
-//            }
-//            map.get(source).add(destination);
-//            map.get(destination).add(source);
-//        }
 
-
-
-//        private static void addEdge(ArrayList<ArrayList<Node>> adj, Node i, Node j) {
-//            adj.get(i).add(j);
-//            adj.get(j).add(i);
-//        }
-//        private int shortestDistanceWrapper(Node s, Node dest, int v){
-//
-//            return shortestDistance(???, s, dest, v);
-//        }
-//        private int shortestDistance(ArrayList<ArrayList<Node>> adj, Node s, Node dest, int v) throws ConnectionDoesNotExistException{
-//            // predecessor[i] array stores predecessor of
-//            // i and distance array stores distance of i
-//            // from s
-//            Node pred[] = new Node[v];
-//            Node dist[] = new Node[v];
-//
-//            if (BFS(adj, s, dest, v, pred, dist) == false) {
-//                throw new ConnectionDoesNotExistException();
-//            }
-//
-//            // LinkedList to store path
-//            LinkedList<Integer> path = new LinkedList<Integer>();
-//            Node crawl = dest;
-//            path.add(crawl);
-//            while (pred[crawl] != -1) {
-//                path.add(pred[crawl]);
-//                crawl = pred[crawl];
-//            }
-//
-//            return dist[dest];
-//        }
-//        private static boolean BFS(ArrayList<ArrayList<Integer>> adj, int src, int dest, int v, int pred[], int dist[]){
-//            LinkedList<Node> queue = new LinkedList<>();
-//            boolean visited[] = new boolean[v];
-//            for (int i = 0; i < v; i++) {
-//                visited[i] = false;
-//                dist[i] = Integer.MAX_VALUE;
-//                pred[i] = -1;
-//            }
-//
-//            visited[src] = true;
-//            dist[src] = 0;
-//            queue.add(src);
-//
-//            while (!queue.isEmpty()) {
-//                int u = queue.remove();
-//                for (int i = 0; i < adj.get(u).size(); i++) {
-//                    if (visited[adj.get(u).get(i)] == false) {
-//                        visited[adj.get(u).get(i)] = true;
-//                        dist[adj.get(u).get(i)] = dist[u] + 1;
-//                        pred[adj.get(u).get(i)] = u;
-//                        queue.add(adj.get(u).get(i));
-//
-//                        // stopping condition (when we find
-//                        // our destination)
-//                        if (adj.get(u).get(i) == dest)
-//                            return true;
-//                    }
-//                }
-//            }
-//            return false;
-//        }
-//        public Graph(List<Person> ppl, ){
-//            Iterator<Person> it = ppl.iterator();
-//            while (it.hasNext()) {
-//                Person temp = it.next();
-//                Iterator<Person> edge_it = temp.getFriends().iterator();
-//                while (edge_it.hasNext()) {
-//                    this.addEdge(new Node(temp), new Node(edge_it.next()));
-//                }
-//            }
-//        }
-//
-//
-//
-//
-//    }
     public static class StatusIteratorImpl implements StatusIterator {
 
         private Iterator<Status> it;
@@ -240,14 +130,22 @@ public class FaceOOPImpl implements FaceOOP, Iterable<Person> {
 //        p_friends_statuses.sort(new ComparePersonsByPopular());
         return new StatusIteratorImpl(p_friends_statuses.iterator());
     }
+
     public Integer rank(Person source, Person target) throws PersonNotInSystemException, ConnectionDoesNotExistException {
 
         if(!this.users.contains(source) || !this.users.contains(target))
             throw new PersonNotInSystemException();
 
+        //
         Queue<Person> q = new LinkedList<>();
         Hashtable<Integer, Boolean> visited = new Hashtable<>();
         Hashtable<Integer, Integer> d = new Hashtable<>();
+
+        int V = this.size();
+        for (Person it : this.users) {
+            d.put(it.getId(), V+1);
+            visited.put(it.getId(), Boolean.FALSE);
+        }
 
         visited.put(source.getId(), Boolean.TRUE);
         d.put(source.getId(), 0);
@@ -262,14 +160,18 @@ public class FaceOOPImpl implements FaceOOP, Iterable<Person> {
             Person u = q.poll();
 
             for (Person temp : u.getFriends()) {
-                if (!visited.containsKey(temp)){
+                if (visited.get(temp.getId()) == Boolean.FALSE){
                     visited.put(temp.getId(), Boolean.TRUE);
-                    d.put(temp.getId(), d.get(u) + 1);
+                    d.put(temp.getId(), d.get(u.getId()) + 1);
                     q.add(temp);
                 }
             }
         }
-        return d.get(target);
+        if (d.get(target.getId()) == V+1){
+            throw new ConnectionDoesNotExistException();
+        }else{
+            return d.get(target.getId());
+        }
     }
 
 
--- src/OOP2/Tests/MainTest.java
+++ src/OOP2/Tests/MainTest.java
@@ -1,74 +1,163 @@
 package OOP2.Tests;
 
 import OOP2.Provided.ConnectionAlreadyExistException;
-import OOP2.Provided.Person;
+import OOP2.Provided.*;
 import OOP2.Provided.SamePersonException;
 import OOP2.Provided.Status;
 import OOP2.Solution.*;
 
+import java.util.Iterator;
+
 public class MainTest {
     public static void main(String[] args) {
-        String p_name = "Yossi";
-        Integer p_id = 1;
-        PersonImpl p = new PersonImpl(p_id, p_name);
-        assert p_name.equals(p.getName());
-        assert p_id.equals(p.getId());
-        assert p.getFriends().size() == 0;
-        PersonImpl p2 = new PersonImpl(p_id, "Yossef");
-        PersonImpl p_bigger_id = new PersonImpl(p_id+1, "Yossef");
-        assert p.equals(p2);
-        assert p.compareTo(p_bigger_id) < 0;
-        int check_exception = 0;
-        try {
-            p.addFriend(p2);
+//        String p_name = "Yossi";
+//        Integer p_id = 1;
+//        PersonImpl p = new PersonImpl(p_id, p_name);
+//        assert p_name.equals(p.getName());
+//        assert p_id.equals(p.getId());
+//        assert p.getFriends().size() == 0;
+//        PersonImpl p2 = new PersonImpl(p_id, "Yossef");
+//        PersonImpl p_bigger_id = new PersonImpl(p_id+1, "Yossef");
+//        assert p.equals(p2);
+//        assert p.compareTo(p_bigger_id) < 0;
+//        int check_exception = 0;
+//        try {
+//            p.addFriend(p2);
+//        }
+//        catch(SamePersonException e) {
+//            check_exception = 1;
+//        }
+//        catch(ConnectionAlreadyExistException e2) {
+//            check_exception = 2;
+//        }
+//        assert check_exception == 1;
+//        check_exception = 0;
+//        try {
+//            p.addFriend(p_bigger_id);
+//        }
+//        catch(SamePersonException e) {
+//            check_exception = 1;
+//        }
+//        catch(ConnectionAlreadyExistException e2) {
+//            check_exception = 2;
+//        }
+//        assert check_exception == 0;
+//        check_exception = 0;
+//        try {
+//            p.addFriend(p_bigger_id);
+//        }
+//        catch(SamePersonException e) {
+//            check_exception = 1;
+//        }
+//        catch(ConnectionAlreadyExistException e2) {
+//            check_exception = 2;
+//        }
+//        assert check_exception == 2;
+//        assert p.getFriends().size() == 1;
+//        for(Person pp: p.getFriends()) {
+//            assert pp.equals(p_bigger_id);
+//        }
+//        p.postStatus("Status1");
+//        p.postStatus("Status2");
+//        p.postStatus("Status3");
+//        p.postStatus("Status4").like(p2);
+//        p.postStatus("Status5").like(p2);
+//        p.postStatus("Status6");
+//        int status_counter = 0;
+//        for(Status st: p.getStatusesRecent()) {
+//            System.out.println("Status number " + st.getId() + " with content " + st.getContent() + "and likes " +st.getLikesCount());
+//            status_counter++;
+//        }
+//        assert status_counter == 6;
+//        for(Status st: p.getStatusesPopular()) {
+//            System.out.println("Status number " + st.getId() + " with content " + st.getContent() + "and likes " +st.getLikesCount());
+//        }
+
+
+        FaceOOP f = new FaceOOPImpl();
+        Person pE = null, pY = null, pD = null, pI = null, pO = null, pS = null, pF = null, pA = null;
+        try{
+            pD = f.joinFaceOOP(3, "Dror");
+            pI = f.joinFaceOOP(4, "Idan");
+            pY = f.joinFaceOOP(1, "Yaniv");
+            pE = f.joinFaceOOP(2, "Eilon");
+
+            f.addFriendship(pY, pE);
+            f.addFriendship(pY, pD);
+            f.addFriendship(pY, pI);
+        } catch (Exception ignored){}
+
+        pY.postStatus("Yaniv");
+        pE.postStatus("Eilon1");
+        pE.postStatus("Eilon2");
+        pD.postStatus("Dror1");
+        pD.postStatus("Dror2").like(pY);
+        pI.postStatus("Idan1");
+        pI.postStatus("Idan3");
+        pI.postStatus("Idan2").like(pE);
+
+        try{
+            for (StatusIterator iter = f.getFeedByPopular(pY); iter.hasNext(); ) {
+                System.out.println(iter.next().getContent());
+            }
+        } catch (Exception e){
+            System.out.println("error:"+e);
         }
-        catch(SamePersonException e) {
-            check_exception = 1;
-        }
-        catch(ConnectionAlreadyExistException e2) {
-            check_exception = 2;
-        }
-        assert check_exception == 1;
-        check_exception = 0;
-        try {
-            p.addFriend(p_bigger_id);
-        }
-        catch(SamePersonException e) {
-            check_exception = 1;
-        }
-        catch(ConnectionAlreadyExistException e2) {
-            check_exception = 2;
-        }
-        assert check_exception == 0;
-        check_exception = 0;
-        try {
-            p.addFriend(p_bigger_id);
-        }
-        catch(SamePersonException e) {
-            check_exception = 1;
-        }
-        catch(ConnectionAlreadyExistException e2) {
-            check_exception = 2;
-        }
-        assert check_exception == 2;
-        assert p.getFriends().size() == 1;
-        for(Person pp: p.getFriends()) {
-            assert pp.equals(p_bigger_id);
-        }
-        p.postStatus("Status1");
-        p.postStatus("Status2");
-        p.postStatus("Status3");
-        p.postStatus("Status4").like(p2);
-        p.postStatus("Status5").like(p2);
-        p.postStatus("Status6");
-        int status_counter = 0;
-        for(Status st: p.getStatusesRecent()) {
-            System.out.println("Status number " + st.getId() + " with content " + st.getContent() + "and likes " +st.getLikesCount());
-            status_counter++;
-        }
-        assert status_counter == 6;
-        for(Status st: p.getStatusesPopular()) {
-            System.out.println("Status number " + st.getId() + " with content " + st.getContent() + "and likes " +st.getLikesCount());
-        }
+
+
+// ------------test rank()------------------
+//        FaceOOP f = new FaceOOPImpl();
+//        Person pE = null, pY = null, pD = null, pI = null, pO = null, pS = null, pF = null, pA = null;
+//        try{
+//            pY = f.joinFaceOOP(1, "Yaniv");
+//            pE = f.joinFaceOOP(2, "Eilon");
+//            pD = f.joinFaceOOP(3, "Dror");
+//            pI = f.joinFaceOOP(4, "Idan");
+//            pO = f.joinFaceOOP(5, "Omer");
+//            pS = f.joinFaceOOP(6, "Shachar");
+//            pF = f.joinFaceOOP(7, "Florentz");
+//            pA = f.joinFaceOOP(8, "Aba");
+//
+//
+//            f.addFriendship(pY, pE);
+//            f.addFriendship(pY, pS);
+//            f.addFriendship(pE, pD);
+//            f.addFriendship(pS, pF);
+//            f.addFriendship(pD, pI);
+//            f.addFriendship(pI, pO);
+//            f.addFriendship(pI, pS);
+//            f.addFriendship(pA, pD);
+////            f.addFriendship(pO, pS);
+//        } catch (Exception ignored){}
+//
+//        int d1=0, d2=0, d3=0, d4=0, d5=0, d6=0, d7=0;
+//        try{
+//            d1 = f.rank(pO, pF);
+//            d2 = f.rank(pY, pF);
+//            d3 = f.rank(pA, pD);
+//            d4 = f.rank(pD, pS);
+//            d5 = f.rank(pA, pS);
+//            d6 = f.rank(pY, pO);
+//            d7 = f.rank(pA, pF);
+//        }catch (Exception ignored){}
+//
+//        assert (d1 == 3);
+//        assert (d2 == 2);
+//        assert (d3 == 1);
+//        assert (d4 == 2);
+//        assert (d5 == 3);
+//        assert (d6 == 3);
+//        assert (d7 == 4);
+//        System.out.println("SUCCES");
+
+
+
+//    Example e = new Example();
+//    e.ExampleTest();
+
+
+
+
+
     }
 }
